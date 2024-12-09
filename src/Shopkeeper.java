89937369a25f4de406c814aff5e07e475e57cec4
TinoLarsen <larsen2099@hotmail.com>
2024-12-09 09:42:56 +0100

    "Player and Shopkeeper"

---
+++ src/Shopkeeper.java
@@ -0,0 +1,27 @@
+public class Shopkeeper {
+
+    private String shopKeeperId;
+    private Double goldBalance = 0.0;
+
+    public Shopkeeper(String shopKeeperId, Double goldBalance) {
+        this.shopKeeperId = shopKeeperId;
+        this.goldBalance = goldBalance;
+    }
+    public String getShopKeeperId(){
+        return shopKeeperId;
+    }
+    public void setShopKeeperId(){
+        this.shopKeeperId = shopKeeperId;
+    }
+    public Double getGoldBalance(){
+        return goldBalance;
+    }
+    public void setGoldBalance(){
+        this.goldBalance = goldBalance;
+    }
+    @Override
+    public String toString(){
+        return "Shopkeeper " +  shopKeeperId + "Shopkeepers Gold " + goldBalance;
+    }
+
+}
