b517e341f4424b9cb97e3a41d1419ce94aabbe3e
Mikkelsen1337 <macztwitch@gmail.com>
2024-12-12 09:21:38 +0100

    Fixed det der "Lorte pis"

--- src/Items/ItemFactory.java
+++ src/Items/ItemFactory.java
@@ -6,17 +6,18 @@ import java.util.Random;
 
 public class ItemFactory {
     private static final List<Item> items = new ArrayList<>();
-/*Fuck det her lorte pikke pisse pis
+
+
     static {
-        items.add(new OneHandWeapon(1,"Sword",8,1,false,0));
-        items.add(new TwoHandsWeapon());
+        items.add(new OneHandWeapon(1, "Sword", 8, 1, false, 0));
+        items.add(new TwoHandsWeapon(2, "Greatsword", 12, 2, false, 0));
         items.add(new Consumable("Potion", 3, 0.5, true, 10, "Heal", 30));
         items.add(new Consumable("Elixir", 4, 0.3, true, 5, "Mana", 20));
-        items.add(new ChestArmor("Platemail"));
-        items.add(new ShoulderArmor("Leather Shoulder Pads"));
-        items.add(new LegsArmor("Leather leggings"));
-        items.add(new Shields("Wooden Shield"));
-        items.add(new HeadArmor("Wooden Helmet"));
+        items.add(new ChestArmor(5, "Platemail", 15, 1, false, 0));
+        items.add(new ShoulderArmor(6, "Leather Shoulder Pads", 5, 1, false, 0));
+        items.add(new LegsArmor(7, "Leather Leggings", 7, 1, false, 0));
+        items.add(new Shields(8, "Wooden Shield", 10, 1, false, 0));
+        items.add(new HeadArmor(9, "Wooden Helmet", 3, 1, false, 0));
     }
 
     public static Item getRandomItem() {
